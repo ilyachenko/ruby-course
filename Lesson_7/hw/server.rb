5dd131823e71de4292d918137e7d4dd3a33336f3
ilya <ilya.dyachenko@gmail.com>
2015-02-13 00:11:06 +0200

    HW 7

--- Lesson_7/hw/server.rb
+++ Lesson_7/hw/server.rb
@@ -1,7 +1,21 @@
 require 'rack'
+require 'cgi'
+require_relative 'svg_dsl.rb'
+
 class Responder
   def call(env)
-    [200, {}, ['Some response body']]
+    params = CGI::parse(env['QUERY_STRING'])
+    res = ''
+    if params.has_key?("x1") && params.has_key?("x2") && params.has_key?("y1") && params.has_key?("y2")
+      line = Line.new(params["x1"][0], params["y1"][0], params["x2"][0], params["y2"][0], "red")
+      res = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
+      res += "\n"
+      res += '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="300">'
+      res += line.draw
+      res += '</svg>'
+    end
+    # http://localhost:8080/?x1=100&y1=10&x2=10&y2=200
+    [200, {'Content-Type' => 'image/svg+xml'}, [res]]
   end
 end
 
--- Lesson_7/hw/svg_dsl.rb
+++ Lesson_7/hw/svg_dsl.rb
@@ -3,35 +3,47 @@ system "clear"
 
 class SVG
   def self.method_missing(name, *args, &block)
-    a = Attr.new
-    a.instance_eval &block
+    attrs_collector = Attr.new
+    block_context = eval 'self', block.binding
+    block_context.instance_variables.each do |var_name|
+      attrs_collector.instance_variable_set(var_name, block_context.instance_variable_get(var_name))
+    end
+    attrs = attrs_collector.instance_eval &block
+    str = ''
+    attrs.each do |key, v|
+      str += " #{key}='#{v}'"
+    end
+    "<#{name}" + str + " />"
   end
 end
 
-
 class Line
-  def initialize(x1,y1,x2,y2,width = 1)
-    @x1, @y1, @x2, @y2, @width = x1, y1, x2, y2, width
+  def initialize(x1,y1,x2,y2,stroke = "black")
+    @x1, @y1, @x2, @y2, @stroke = x1, y1, x2, y2, stroke
   end
 
   def draw
-    p @x1
     SVG.line do
       x1 @x1
       y1 @y1
+      x2 @x2
+      y2 @y2
+      stroke @stroke
     end
   end
 end
 
 class Attr
-  attr_reader :attrs
+  def initialize
+    @attrs = {}
+  end
+
   def method_missing(name, v)
-    p name
-    p v
-    # @attrs[name] = v[0]
+    @attrs[name] = v
+    @attrs
   end
 end
 
-line = Line.new(50, 160, 50, 220)
+# line = Line.new(50, 160, 50, 220)
 
-p line.draw # "<line x1='50' y1='160' x2='50' y2='220'/>"
+# p line.draw # "<line x1='50' y1='160' x2='50' y2='220'/>"
